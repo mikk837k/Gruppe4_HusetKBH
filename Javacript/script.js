359b627b48eb6b3a46b7ed9c85e4b649c395f4e6
Mikkel Madsen <mikkelmadsen@mikkels-air.home>
2018-11-08 20:59:27 +0100

    singelview side tilføjet med lidt styling. Javascript til singelview. test_index.html indeholder nødvendig kode til index side for at singelview virker.

--- Javacript/script.js
+++ Javacript/script.js
@@ -1,28 +1,35 @@
+let myPosts;
+//let myTemplate = document.querySelector("#data_template");
+//Laver søgning i URL'en
+let urlPrams = new URLSearchParams(window.location.search);
+//Henter værdien "id" fra URL'en hvis den er tilstede
+let id = urlPrams.get("id");
+console.log(id);
+
 document.addEventListener("DOMContentLoaded", getJSON);
 
-let myPosts;
-let destination = document.querySelector(".data_content");
-
-
+//Henter data og sikre at det læses som JSON
 async function getJSON() {
 
-    let myJSON = await fetch("http://designhagenow.dk/kea/huset-kbh/wordpress/wp-json/wp/v2/faciliteter");
+    let myJSON = await fetch("https://designhagenow.dk/kea/huset-kbh/wordpress/wp-json/wp/v2/faciliteter");
     myPosts = await myJSON.json();
+
     console.log(myPosts);
-    showPosts();
+    showFacility();
 }
 
-function showPosts() {
-    let myTemplate = document.querySelector("#data_template");
 
-    console.log("showPosts kørt");
-
+function showFacility() {
+    let dest = document.querySelector(".singelview_data_content");
     myPosts.forEach(post => {
-        let klon = myTemplate.cloneNode(true).content;
-        klon.querySelector("img").src = post.acf.billede;
-        klon.querySelector("h2").innerHTML = post.acf.titel;
-        klon.querySelector(".data_textarea").innerHTML = post.acf.tekst;
-        klon.querySelector(".data_infobox").innerHTML = post.acf.info;
-        destination.appendChild(klon);
+        console.log(post.id);
+        if (post.id == id) {
+            dest.querySelector("img").src = post.acf.billede;
+            dest.querySelector("h2").innerHTML = post.acf.titel;
+            dest.querySelector(".data_textarea").innerHTML = post.acf.tekst;
+            dest.querySelector(".data_openinghours").innerHTML = post.acf.aabningstider;
+            dest.querySelector(".data_url").href = post.acf.link_URL;
+        }
+        console.log("showPosts er kørt")
     })
 }
